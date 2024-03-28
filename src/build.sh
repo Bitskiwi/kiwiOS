671b456d059c6ee0906db05ed012cf91b88b0d90
bitkiwi <varkiwibusiness@gmail.com>
2024-03-27 22:51:10 -0700

    im lazy and bored switch to wiki.osdev.org tutorial now

--- src/build.sh
+++
@@ -1,8 +0,0 @@
-# Compile assembly files
-nasm -f bin boot.asm -o boot.bin
-nasm -f bin kernel.asm -o kernel.bin
-
-# Create a disk image
-dd if=/dev/zero of=os.img bs=512 count=2880  # Create a 1.44 MB file
-dd if=boot.bin of=os.img bs=512 conv=notrunc
-dd if=kernel.bin of=os.img bs=512 seek=1 conv=notrunc
--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,10 @@
-. build.sh
-qemu-system-i386 os.img
+as --32 boot.s -o boot.o
+gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+gcc -T linker.ld -o kiwiOS.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc -m32
+
+mkdir -p iso/boot/grub
+cp kiwiOS.bin iso/boot/kiwiOS.bin
+cp grub.cfg iso/boot/grub/grub.cfg
+grub-mkrescue -o kiwiOS.iso iso
+
+qemu-system-i386 -cdrom kiwiOS.iso
