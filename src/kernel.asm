719c41be86e4f65c0b436e448418f4f6ca89c980
bitkiwi <varkiwibusiness@gmail.com>
2024-04-08 13:02:33 -0700

    print and input etc.

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,43 +1,69 @@
 org 0x7e00
 
 kernel_main:
+	call cls
 	mov si, KERNEL_MSG
-	call println
+	call outln
+	call in
 
 	jmp $
 
 KERNEL_MSG: db "Kernel :)", 0
 
 CRLF:
+	pusha
 	mov ah, 0x0e
 	mov al, 0x0a
 	int 0x10
 	mov al, 0x0d
 	int 0x10
-	ret
+	.end:
+		popa
+		ret
 
-printc:
+cout:
 	mov ah, 0x0e
 	int 0x10
-	ret
+	.end:
+		ret
 
-print:
+out:
 	.loop:
 		mov al, [si]
 		cmp al, 0
 		je .end
-		call printc
+		call cout
 		inc si
 		jmp .loop
 	.end:
 		ret
 
-println:
-	call print
+outln:
+	call out
 	call CRLF
-	ret
+	.end:
+		ret
+
+cin:
+	mov ah, 0x00
+	int 0x16
+	.end:
+		ret
+
+in:
+	.loop:
+		call cin
+		;mov al, [ah]
+		cmp al, 10
+		je .end
+		call cout
+		jmp .loop
+	.end:
+		ret
 
 cls:
-	mov ah, 0x06
+	mov ah, 0x00
+	mov al, 0x03
 	int 0x10
-	ret
+	.end:
+		ret
